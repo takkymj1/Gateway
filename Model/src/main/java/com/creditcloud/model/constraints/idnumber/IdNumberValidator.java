497a189fa57bbd62c92afdcdb0e0793f3ad7c95f
rooseek <rooseek@gmail.com>
2013-07-07 18:44:50 +0800

    complete complex validation for annotation IdNumber

---
+++ Model/src/main/java/com/creditcloud/model/constraints/idnumber/IdNumberValidator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.model.constraints.idnumber;
+
+import com.creditcloud.model.constraints.IdNumber;
+import javax.validation.ConstraintValidator;
+
+/**
+ *
+ * @author rooseek
+ */
+public interface IdNumberValidator extends ConstraintValidator<IdNumber, String> {
+    
+}
