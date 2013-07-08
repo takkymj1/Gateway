b867ef82d7f28b99d9825bcae0c749ef660aaffd
rooseek <rooseek@gmail.com>
2013-07-08 16:25:15 +0800

    various fix for IdNumber and Url

---
+++ Model/src/main/java/com/creditcloud/model/constant/IdNumberConstant.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.model.constant;
+
+/**
+ *
+ * @author rooseek
+ */
+public interface IdNumberConstant {
+
+    //earliest year the idNumber could use, too old!
+    int MIN_YEAR = 1900;
+}
