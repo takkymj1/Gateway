d33a50029bfbc74d4378bd64751ce9aab986bb81
rooseek <rooseek@gmail.com>
2013-07-23 17:17:28 +0800

    add comment for methods call in LoanService

--- Service/src/main/java/com/creditcloud/service/LoanService.java
+++ Service/src/main/java/com/creditcloud/service/LoanService.java
@@ -19,11 +19,11 @@ public interface LoanService {
      *
      * @param clientCode
      * @param loanRequest
-     * @return true if submit request successful, false if not
+     * @return 0 for failure, 1 for success, 2 for exceed 3 submit in one day
      * @throw ClientCodeNotMatchException if incoming client code do not match
      * the local client
      */
-    boolean submitLoanRequest(String clientCode, LoanRequest loanRequest);
+    int submitLoanRequest(String clientCode, LoanRequest loanRequest);
 
     /**
      * list loans by user id
