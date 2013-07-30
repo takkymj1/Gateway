ac858d1c25164afb13da446991cddf3ceb5a5b13
rooseek <rooseek@gmail.com>
2013-07-30 10:52:52 +0800

    move DateUtils from CreditManager to Common

--- Common/src/test/java/com/creditcloud/common/utils/DateUtilsTest.java
+++ Common/src/test/java/com/creditcloud/common/utils/DateUtilsTest.java
@@ -8,6 +8,7 @@ import com.creditcloud.model.misc.Duration;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Set;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,24 +21,24 @@ import static org.junit.Assert.*;
  * @author sobranie
  */
 public class DateUtilsTest {
-    
+
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-    
+
     public DateUtilsTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -53,5 +54,25 @@ public class DateUtilsTest {
         Date expResult = sdf.parse("20120229");
         Date result = DateUtils.offset(asOfDate, duration);
         assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of listDates method, of class DateUtils.
+     */
+    @Test
+    public void testListDates() throws ParseException {
+
+        Date start = sdf.parse("20130601");
+        Date end = sdf.parse("20130630");
+        Set result = DateUtils.listDates(start, end);
+        assertEquals(result.size(), 30);
+
+        start = sdf.parse("20130630");
+        result = DateUtils.listDates(start, end);
+        assertEquals(result.size(), 1);
+
+        start = sdf.parse("20130730");
+        result = DateUtils.listDates(start, end);
+        assertEquals(result.size(), 0);
     }
 }
