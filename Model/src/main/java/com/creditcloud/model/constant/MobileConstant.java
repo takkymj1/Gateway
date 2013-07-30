7082d412124a5983f3237be5760c8ff97a5cd02f
sobranie9 <sobranie9@gmail.com>
2013-07-30 23:46:20 +0800

    added default mobile as constant

---
+++ Model/src/main/java/com/creditcloud/model/constant/MobileConstant.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.model.constant;
+
+/**
+ *
+ * @author sobranie
+ */
+public interface MobileConstant {
+
+    /**
+     * default mobile number for admin user, sorry China Mobile...
+     */
+    String DEFAULT_MOBILE = "13800138000";
+}
