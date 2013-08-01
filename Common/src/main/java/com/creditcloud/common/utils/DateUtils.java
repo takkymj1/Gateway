274f79ff084abbeb352a4d9e317ce6775623de78
rooseek <rooseek@gmail.com>
2013-08-01 14:47:58 +0800

    add method to get the 0 o'clock time for a date

--- Common/src/main/java/com/creditcloud/common/utils/DateUtils.java
+++ Common/src/main/java/com/creditcloud/common/utils/DateUtils.java
@@ -4,7 +4,9 @@
  */
 package com.creditcloud.common.utils;
 
+import com.creditcloud.model.constant.TimeConstant;
 import com.creditcloud.model.misc.Duration;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -47,5 +49,24 @@ public class DateUtils {
         }
 
         return dates;
+    }
+
+    /**
+     * return the 0'clock time for a date, like 2013/8/1 0:0:0
+     *
+     * @param date
+     * @return
+     */
+    public static Date get0OClock(Date date) {
+        if (date == null) {
+            return null;
+        }
+
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        calendar.set(year, month, day, 0, 0, 0);
+        return calendar.getTime();
     }
 }
