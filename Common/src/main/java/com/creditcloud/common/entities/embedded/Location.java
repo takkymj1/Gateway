cc9da3ba69ceb0156766e2b051f3fd4ab9c83e80
sobranie9 <sobranie9@gmail.com>
2013-08-09 15:53:37 +0800

    added Location

---
+++ Common/src/main/java/com/creditcloud/common/entities/embedded/Location.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.common.entities.embedded;
+
+import com.creditcloud.common.entities.BaseEntity;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * 地址位置信息，为分支机构服务地址或者实地信审用地址
+ * 
+ * @author sobranie
+ */
+@Embeddable
+public class Location extends BaseEntity {
+
+    @Column(nullable = false)
+    private String province;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String streetAddress;
+
+    private String roomAddress;
+
+    private BigDecimal longtitude;
+
+    private BigDecimal latitude;
+    
+    public Location() {
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getRoomAddress() {
+        return roomAddress;
+    }
+
+    public void setRoomAddress(String roomAddress) {
+        this.roomAddress = roomAddress;
+    }
+
+    public BigDecimal getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(BigDecimal longtitude) {
+        this.longtitude = longtitude;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+}
