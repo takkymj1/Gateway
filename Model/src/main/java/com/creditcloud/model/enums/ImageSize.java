cb691a9c12c41ff827fd3e12dc2bd8b6c1036926
rooseek <rooseek@gmail.com>
2013-09-04 19:03:10 +0800

    fix ImageStore, rename Realm

--- Model/src/main/java/com/creditcloud/model/enums/ImageSize.java
+++ Model/src/main/java/com/creditcloud/model/enums/ImageSize.java
@@ -10,12 +10,10 @@ package com.creditcloud.model.enums;
  */
 public enum ImageSize implements BaseEnum {
 
-    //TODO add more
-    LARGE(""),
-    MEDIUM(""),
+    //需要在upyun中创建缩略图和水印版本然后再定义
     ORIGINAL("原图"),
-    SMALL(""),
-    ICON("");
+    SMALL("小图"),
+    ICON("图标");
 
     private final String key;
 
