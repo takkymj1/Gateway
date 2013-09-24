bd6e845e87453380a8e9d8298fdf14bd6cc16066
sobranie9 <sobranie9@gmail.com>
2013-09-24 14:25:51 +0800

    CMKT-11 增加提现申请状态，增加BankAccountTag

--- Model/src/main/java/com/creditcloud/model/enums/user/fund/WithdrawStatus.java
+++ Model/src/main/java/com/creditcloud/model/enums/user/fund/WithdrawStatus.java
@@ -7,13 +7,15 @@ package com.creditcloud.model.enums.user.fund;
 import com.creditcloud.model.enums.BaseEnum;
 
 /**
- *
+ * 提现申请状态
+ * 
  * @author rooseek
  */
 public enum WithdrawStatus implements BaseEnum {
 
     FAILED("失败"),
     PROCESSING("处理中"),
+    REJECTED("被拒绝"),
     SUCCESSFUL("成功");
 
     private final String key;
