50c6c4c12f55b1fa60a5b5feacffec7f7483b9ac
sobranie9 <sobranie9@gmail.com>
2013-10-08 02:05:31 +0800

    将Role转化为标准Java Bean

--- Model/src/main/java/com/creditcloud/model/client/Role.java
+++ Model/src/main/java/com/creditcloud/model/client/Role.java
@@ -4,6 +4,7 @@
  */
 package com.creditcloud.model.client;
 
+import com.creditcloud.model.BaseObject;
 import com.creditcloud.model.enums.client.Privilege;
 import java.util.Collection;
 import javax.validation.constraints.NotNull;
@@ -15,24 +16,27 @@ import javax.validation.constraints.NotNull;
  *
  * @author sobranie
  */
-public class Role {
-
-    private static final long serialVersionUID = 20130918L;
+public class Role extends BaseObject {
 
     @NotNull
-    private final String name;
+    private String name;
 
     /**
      * 拥有该权限的所有员工
      */
-    private final Collection<Employee> members;
+    private Collection<Employee> members;
 
     /**
      * 该角色所拥有的权限集合
      */
-    private final Collection<Privilege> privileges;
-
-    public Role(String name, Collection<Employee> members, Collection<Privilege> privileges) {
+    private Collection<Privilege> privileges;
+    
+    public Role() {
+    }
+    
+    public Role(String name,
+                Collection<Employee> members,
+                Collection<Privilege> privileges) {
         this.name = name;
         this.members = members;
         this.privileges = privileges;
@@ -48,5 +52,17 @@ public class Role {
 
     public Collection<Privilege> getPrivileges() {
         return privileges;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMembers(Collection<Employee> members) {
+        this.members = members;
+    }
+
+    public void setPrivileges(Collection<Privilege> privileges) {
+        this.privileges = privileges;
     }
 }
