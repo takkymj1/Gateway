28bb7054032dfb5699d27567c0424ac03fd68404
sobranie9 <sobranie9@gmail.com>
2013-10-19 04:07:54 +0800

    rename BranchType, added blank BranchService

---
+++ Client/src/main/java/com/creditcloud/client/api/BranchService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.creditcloud.client.api;
+
+import javax.ejb.Remote;
+
+/**
+ * 组织机构管理接口
+ * 
+ * @author sobranie
+ */
+@Remote
+public interface BranchService {
+    
+}
