2d4f85424750bd1dd3e9e094bfacaf7612630530
sobranie9 <sobranie9@gmail.com>
2013-11-11 00:14:18 +0800

    fix UserUnFreezeRequest

--- Payment/src/main/java/com/creditcloud/payment/model/chinapnr/UserUnFreezeRequest.java
+++ Payment/src/main/java/com/creditcloud/payment/model/chinapnr/UserUnFreezeRequest.java
@@ -6,6 +6,7 @@
 package com.creditcloud.payment.model.chinapnr;
 
 import com.creditcloud.payment.model.chinapnr.base.BaseRequest;
+import com.creditcloud.payment.model.chinapnr.enums.CmdIdType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import org.apache.commons.lang3.StringUtils;
