32a77ae4dd331d2ad8ff76b19b467f7a6e473e1b
sobranie9 <sobranie9@gmail.com>
2013-11-20 03:37:05 +0800

    added SocialConstant

---
+++ User/src/main/java/com/creditcloud/user/social/SocialConstant.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.user.social;
+
+/**
+ *
+ * @author sobranie
+ */
+public interface SocialConstant {
+
+    /**
+     * 在Social中的名称
+     */
+    String SocialName = "SocialName";
+}
