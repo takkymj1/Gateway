d114521fd6bd5baf3f068c5db9835a633d1486b7
sobranie9 <sobranie9@gmail.com>
2013-11-23 18:28:15 +0800

    alter Contract

--- Contract/src/main/java/com/creditcloud/contract/api/ContractService.java
+++ Contract/src/main/java/com/creditcloud/contract/api/ContractService.java
@@ -8,9 +8,9 @@ package com.creditcloud.contract.api;
 import com.creditcloud.config.FeeConfig;
 import com.creditcloud.contract.Contract;
 import com.creditcloud.model.client.Client;
+import com.creditcloud.model.loan.Invest;
 import com.creditcloud.model.loan.Loan;
 import com.creditcloud.model.loan.LoanRepayment;
-import com.creditcloud.model.user.User;
 import java.util.List;
 import javax.ejb.Remote;
 
@@ -26,16 +26,14 @@ public interface ContractService {
      * 生成一份普通的平台借款合同
      *
      * @param client 平台
-     * @param lender 投资人/出借人
-     * @param obligator 借款人
+     * @param invest 投资
      * @param loan 借款对象
      * @param repayments 还款列表
      * @param feeConfig 费用配置
      * @return
      */
     Contract generateLoanContract(Client client,
-                                  User lender,
-                                  User obligator,
+                                  Invest invest,
                                   Loan loan,
                                   List<LoanRepayment> repayments,
                                   FeeConfig feeConfig);
