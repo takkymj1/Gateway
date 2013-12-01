a04232bea83aacc8ef3bf5482a2e9eec0c7a9caa
sobranie9 <sobranie9@gmail.com>
2013-12-01 22:44:45 +0800

    make generateLoanContract async

--- Contract/src/main/java/com/creditcloud/contract/api/ContractService.java
+++ Contract/src/main/java/com/creditcloud/contract/api/ContractService.java
@@ -23,19 +23,20 @@ import javax.ejb.Remote;
 public interface ContractService {
 
     /**
-     * 生成一份普通的平台借款合同
+     * 生成一份普通的平台借款合同.
+     * 
+     * 只是触发远程的生成过程，为异步调用
      *
      * @param client 平台
      * @param invest 投资
      * @param loan 借款对象
      * @param repayments 还款列表
      * @param feeConfig 费用配置
-     * @return
      */
-    Contract generateLoanContract(Client client,
-                                  Invest invest,
-                                  Loan loan,
-                                  List<LoanRepayment> repayments,
-                                  FeeConfig feeConfig);
+    void generateLoanContract(Client client,
+                              Invest invest,
+                              Loan loan,
+                              List<LoanRepayment> repayments,
+                              FeeConfig feeConfig);
 
 }
