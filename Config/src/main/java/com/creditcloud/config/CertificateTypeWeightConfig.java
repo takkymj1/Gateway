3e1efbe5faf98509e26131e8961239c7d5175057
suetming <suetming.ma@gmail.com>
2013-12-10 17:17:54 +0800

    add certificateType weight config

---
+++ Config/src/main/java/com/creditcloud/config/CertificateTypeWeightConfig.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.config;
+
+import com.creditcloud.model.enums.user.credit.CertificateType;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * 
+ * @author suetming
+ */
+@XmlRootElement(name = "CertificateTypeWeightConfig")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CertificateTypeWeightConfig extends BaseConfig {
+
+    private static final long serialVersionUID = 20131105L;
+
+    public static final String CONFIG_NAME = "CertificateTypeWeightConfig";
+   
+    @XmlElement(name = "CertificateType", required = true)
+    private String type;
+    
+    public HashMap<String, BigDecimal> getCertificateTypeWeight(){
+        HashMap<String, BigDecimal> weightMap = new HashMap<>();
+        String[] weights = type.split(" ");
+        
+        CertificateType[] values = CertificateType.values();
+        for(int i = 0, n = values.length, m = weights.length; i < n; i++){
+            if (i > m - 1) {
+                BigDecimal weight = new BigDecimal(weights[i]);
+                weightMap.put(values[i].name(), weight);
+            } else {
+                BigDecimal weight = new BigDecimal(0);
+                weightMap.put(values[i].name(), weight);
+            }
+        }
+        return weightMap;
+    }
+}
