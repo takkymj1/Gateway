8883bdde2f2bfe415ebb87c64bd79a0b52c30b42
rooseek <rooseek@gmail.com>
2013-12-17 16:42:53 +0800

    UND-26 add ActivityService , remove other activity code

---
+++ Activity/src/main/java/com/creditcloud/activity/ActivityType.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.activity;
+
+import com.creditcloud.model.enums.BaseEnum;
+import com.creditcloud.model.enums.Realm;
+import com.creditcloud.model.enums.loan.LoanRequestStatus;
+import com.creditcloud.model.enums.loan.LoanStatus;
+
+/**
+ * 活动类型，按照realm分类
+ *
+ * @author rooseek
+ */
+public enum ActivityType implements BaseEnum {
+
+    /**
+     * 贷款申请类活动
+     */
+    REQUEST_SUMBIT("提交贷款申请", Realm.LOANREQUEST),
+    REQUEST_ASSIGN(LoanRequestStatus.ASSIGNED.getKey(), Realm.LOANREQUEST),
+    REQUEST_ASSIGN_VISIT_TASK("分配外审任务", Realm.LOANREQUEST),
+    REQUEST_VISIT_REVIEW("外勤评审", Realm.LOANREQUEST),
+    REQUEST_ASSIGN_RISK_TASK("分配风控任务", Realm.LOANREQUEST),
+    REQUEST_RISK_REVIEW("风控评审", Realm.LOANREQUEST),
+    REQUEST_CANCELL(LoanRequestStatus.CANCELED.getKey(), Realm.LOANREQUEST),
+    REQUEST_APPROVE(LoanRequestStatus.APPROVED.getKey(), Realm.LOANREQUEST),
+    REQUEST_PUBLISH(LoanRequestStatus.PUBLISHED.getKey(), Realm.LOANREQUEST),
+    REQUEST_REJECT(LoanRequestStatus.REJECTED.getKey(), Realm.LOANREQUEST),
+    /**
+     * 贷款类活动
+     */
+    LOAN_SPLIT("拆标", Realm.LOAN),
+    LOAN_SCHEDULE(LoanStatus.SCHEDULED.getKey(), Realm.LOAN),
+    LOAN_OPEN(LoanStatus.OPENED.getKey(), Realm.LOAN),
+    LOAN_FAIL(LoanStatus.FAILED.getKey(), Realm.LOAN),
+    LOAN_CANCEL(LoanStatus.CANCELED.getKey(), Realm.LOAN),
+    LOAN_FINISH(LoanStatus.FINISHED.getKey(), Realm.LOAN),
+    LOAN_SETTLE(LoanStatus.SETTLED.getKey(), Realm.LOAN),
+    LOAN_CLEAR(LoanStatus.CLEARED.getKey(), Realm.LOAN),
+    LOAN_OVERDUE(LoanStatus.OVERDUE.getKey(), Realm.LOAN),
+    LOAN_BREACH(LoanStatus.BREACH.getKey(), Realm.LOAN);
+
+    private final String key;
+
+    private final Realm realm;
+
+    private ActivityType(String key, Realm realm) {
+        this.key = key;
+        this.realm = realm;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    public Realm getRealm() {
+        return realm;
+    }
+}
--- Activity/src/main/java/com/creditcloud/activity/api/ActivityService.java
+++ Activity/src/main/java/com/creditcloud/activity/api/ActivityService.java
@@ -4,6 +4,10 @@
  */
 package com.creditcloud.activity.api;
 
+import com.creditcloud.activity.Activity;
+import com.creditcloud.model.criteria.PageInfo;
+import com.creditcloud.model.misc.PagedResult;
+import com.creditcloud.model.misc.RealmEntity;
 import javax.ejb.Remote;
 
 /**
@@ -12,5 +16,37 @@ import javax.ejb.Remote;
  */
 @Remote
 public interface ActivityService {
-    
+
+    /**
+     *
+     * @param clientCode
+     * @param activity
+     */
+    void addActivity(String clientCode, Activity activity);
+
+    /**
+     *
+     * @param clientCode
+     * @param activityId
+     * @return
+     */
+    Activity getById(String clientCode, String activityId);
+
+    /**
+     * 按照活动关联对象列出活动
+     *
+     * @param clientCode
+     * @param target
+     * @return
+     */
+    PagedResult<Activity> listByTarget(String clientCode, RealmEntity target, PageInfo pageInfo);
+
+    /**
+     * 按活动发起者列出活动
+     *
+     * @param clientCode
+     * @param performer
+     * @return
+     */
+    PagedResult<Activity> listByPerformer(String clientCode, RealmEntity performer, PageInfo pageInfo);
 }
