f79e6162ef10a43b6917c861e781734a3ad41065
rooseek <hao.chen@creditcloud.com>
2013-12-18 20:37:48 +0800

    fix fee related

---
+++ Model/src/main/java/com/creditcloud/model/loan/OverduePenalty.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.model.loan;
+
+import com.creditcloud.model.BaseObject;
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * 逾期滞纳金
+ *
+ * @author rooseek
+ */
+@XmlRootElement
+public class OverduePenalty extends BaseObject {
+
+    public static final OverduePenalty NONE = new OverduePenalty(BigDecimal.ZERO, BigDecimal.ZERO);
+
+    private static final long serialVersionUID = 20131218L;
+
+    /**
+     * 逾期管理费，如一次性收取应还金额的5%,也可能是每日收取
+     */
+    private BigDecimal overdue;
+
+    /**
+     * 逾期罚息，如每日收取应还金额的0.05%
+     */
+    private BigDecimal penalty;
+
+    public OverduePenalty() {
+    }
+
+    public OverduePenalty(BigDecimal overdue, BigDecimal penalty) {
+        this.overdue = overdue;
+        this.penalty = penalty;
+    }
+
+    public BigDecimal getOverdue() {
+        return overdue;
+    }
+
+    public BigDecimal getPenalty() {
+        return penalty;
+    }
+
+    public void getTotal() {
+        overdue.add(penalty);
+    }
+
+    public void setOverdue(BigDecimal overdue) {
+        this.overdue = overdue;
+    }
+
+    public void setPenalty(BigDecimal penalty) {
+        this.penalty = penalty;
+    }
+}
