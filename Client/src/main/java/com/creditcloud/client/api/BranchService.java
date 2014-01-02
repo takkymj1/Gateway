ac87c223f4c7ccf3c39b164322eecfab5d7de83b
rooseek <rooseek@gmail.com>
2014-01-03 00:55:25 +0800

    add branch method

--- Client/src/main/java/com/creditcloud/client/api/BranchService.java
+++ Client/src/main/java/com/creditcloud/client/api/BranchService.java
@@ -3,17 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.creditcloud.client.api;
 
+import com.creditcloud.model.client.Branch;
 import javax.ejb.Remote;
 
 /**
  * 组织机构管理接口
- * 
+ *
  * @author sobranie
  */
 @Remote
 public interface BranchService {
-    
+
+    /**
+     * 添加新的Branch
+     *
+     * @param clientCode
+     * @param branch
+     * @return
+     */
+    Branch addNew(String clientCode, Branch branch);
+
+    /**
+     * 更新现有branch
+     *
+     * @param clientCode
+     * @param branch
+     * @return
+     */
+    Branch update(String clientCode, Branch branch);
 }
