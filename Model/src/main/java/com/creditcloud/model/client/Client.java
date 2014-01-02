e95b47e6a168543824e58affc209b4d815459494
sobranie9 <sobranie9@gmail.com>
2014-01-03 02:56:55 +0800

    added FormParam to Client

--- Model/src/main/java/com/creditcloud/model/client/Client.java
+++ Model/src/main/java/com/creditcloud/model/client/Client.java
@@ -11,6 +11,7 @@ import com.creditcloud.model.constraints.EmailAddress;
 import com.creditcloud.model.constraints.MobileNumber;
 import java.util.Locale;
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.FormParam;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,42 +31,49 @@ public class Client extends BaseObject {
     /**
      * full name of the client
      */
+    @FormParam("name")
     @NotNull
     protected String name;
 
     /**
      * 客户简称
      */
+    @FormParam("shortName")
     @NotNull
     private String shortName;
     
     /**
      * 显示在Market上的title
      */
+    @FormParam("title")
     @NotNull
     private String title;
     
     /**
      * 系统使用的手机，用于发送重要消息
      */
+    @FormParam("mobile")
     @MobileNumber
     private String mobile;
     
     /**
      * 技术支持邮件，在异常等状况下应用
      */
+    @FormParam("supportEmail")
     @EmailAddress
     private String supportEmail;
 
     /**
      * 4 characters codes 唯一标识
      */
+    @FormParam("code")
     @ClientCode
     protected String code;
 
     /**
      * url
      */
+    @FormParam("url")
     @ClientURL
     protected String url;
 
