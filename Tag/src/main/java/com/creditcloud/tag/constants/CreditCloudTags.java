80a0d600eb51694e5405ab048694600a2e70114e
rooseek <rooseek@gmail.com>
2014-01-09 17:05:38 +0800

    some fix with fund and tag

--- Tag/src/main/java/com/creditcloud/tag/constants/CreditCloudTags.java
+++ Tag/src/main/java/com/creditcloud/tag/constants/CreditCloudTags.java
@@ -17,26 +17,26 @@ public interface CreditCloudTags {
     /**
      * VIP客户
      */
-    Tag VIP_USER = new Tag(Realm.STRING, "VIP", "贵宾客户");
+    Tag VIP_USER = new Tag(Realm.STRING, "VIP", "VIP", "贵宾客户");
 
     /**
      * 平台运营方相关的内部客户
      */
-    Tag INTERNAL_USER = new Tag(Realm.STRING, "内部用户", "内部用户客户");
+    Tag INTERNAL_USER = new Tag(Realm.STRING, "内部用户", "内部用户", "内部用户客户");
 
     /**
      * 个人用途借款
      */
-    Tag LOAN_PERSONAL = new Tag(Realm.STRING, "个人借款", "个人借款");
+    Tag LOAN_PERSONAL = new Tag(Realm.STRING, "个人借款", "个人借款", "个人借款");
 
     /**
      * 个人代表企业借款
      */
-    Tag LOAN_COMPANY = new Tag(Realm.STRING, "企业借款", "个人代表企业借款");
+    Tag LOAN_COMPANY = new Tag(Realm.STRING, "企业借款", "企业借款", "个人代表企业借款");
 
     /**
      * 个人代销理财产品
      */
-    Tag LOAN_WEALTH = new Tag(Realm.STRING, "理财产品", "个人代销理财产品");
+    Tag LOAN_WEALTH = new Tag(Realm.STRING, "理财产品", "理财产品", "个人代销理财产品");
 
 }
--- Tag/src/main/java/com/creditcloud/tag/model/Tag.java
+++ Tag/src/main/java/com/creditcloud/tag/model/Tag.java
@@ -9,10 +9,8 @@ import com.creditcloud.model.enums.Realm;
 import com.creditcloud.tag.constants.TagConstant;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 /**
  * 可以自定义的标签
@@ -20,9 +18,7 @@ import lombok.NoArgsConstructor;
  * @author rooseek
  */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(exclude = {"description"})
+@EqualsAndHashCode(exclude = {"description", "alias"})
 public class Tag extends BaseObject {
 
     private static final long serialVersionUID = 20131230L;
@@ -40,8 +36,24 @@ public class Tag extends BaseObject {
     @Size(max = TagConstant.MAX_TAG_NAME)
     private String name;
 
+    /**
+     * tag的别名，可以不唯一
+     */
+    @Size(max = TagConstant.MAX_TAG_NAME)
+    private String alias;
+
     @Size(max = TagConstant.MAX_TAG_DESCRIPTION)
     private String description;
+
+    public Tag() {
+    }
+
+    public Tag(Realm realm, String name, String alias, String description) {
+        this.realm = realm;
+        this.name = name;
+        this.alias = alias;
+        this.description = description;
+    }
 
     @Override
     public String toString() {
@@ -49,6 +61,6 @@ public class Tag extends BaseObject {
     }
 
     public static Tag valueOf(Realm realm, String tagName) {
-        return new Tag(realm, tagName, null);
+        return new Tag(realm, tagName, null, null);
     }
 }
