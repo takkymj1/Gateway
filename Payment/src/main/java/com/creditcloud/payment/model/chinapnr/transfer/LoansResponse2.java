fd5422d42893ca4d950ee5b9173bbbdc0ef432b2
rooseek <rooseek@gmail.com>
2014-01-28 21:53:36 +0800

    汇付2.0接口

--- Payment/src/main/java/com/creditcloud/payment/model/chinapnr/transfer/LoansResponse2.java
+++ Payment/src/main/java/com/creditcloud/payment/model/chinapnr/transfer/LoansResponse2.java
@@ -35,10 +35,6 @@ public class LoansResponse2 extends BaseResponse {
     @NotNull
     @Size(max = 16)
     private String OutCustId;
-    
-    private String SubOrdId;
-    
-    private String SubOrdDate;
 
     @FormParam("OutAcctId")
     @Size(max = 9)
