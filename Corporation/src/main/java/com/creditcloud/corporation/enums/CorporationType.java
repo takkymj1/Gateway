32194b500acca4fb0b47d8e151bb9ef4344fde77
rooseek <rooseek@gmail.com>
2014-02-18 11:19:08 +0800

    add skeleton CorporationService

---
+++ Corporation/src/main/java/com/creditcloud/corporation/enums/CorporationType.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.corporation.enums;
+
+import com.creditcloud.model.enums.BaseEnum;
+
+/**
+ *
+ * @author rooseek
+ */
+public enum CorporationType implements BaseEnum {
+
+    GENERAL("一般公司"),
+    GUARANTEE("担保公司");
+
+    private final String key;
+
+    private CorporationType(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+}
