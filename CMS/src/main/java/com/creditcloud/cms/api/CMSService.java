d78958fa1bdebab867389915d6b1457939672680
wusijia <wusijia1987@gmail.com>
2014-03-12 18:10:19 +0800

    page

--- CMS/src/main/java/com/creditcloud/cms/api/CMSService.java
+++ CMS/src/main/java/com/creditcloud/cms/api/CMSService.java
@@ -8,6 +8,7 @@ package com.creditcloud.cms.api;
 import com.creditcloud.cms.constant.Category;
 import com.creditcloud.cms.model.Article;
 import com.creditcloud.cms.model.Channel;
+import com.creditcloud.model.criteria.PageInfo;
 import com.creditcloud.model.misc.PagedResult;
 import javax.ejb.Remote;
 
@@ -31,27 +32,27 @@ public interface CMSService {
     /**
      * 通过类别列出文章
      */
-    PagedResult<Article> listByCategory(Category category);
+    PagedResult<Article> listByCategory(Category category, PageInfo pageInfo);
 
     /**
      * 通过类别列出文章
      */
-    Article listBySingle(Category category);
+    Article getBySingle(Category category);
 
     /**
      * 通过栏目列出文章
      */
-    PagedResult<Article> listByChannel(String channelName);
+    PagedResult<Article> listByChannel(String channelName, PageInfo pageInfo);
 
     /**
      * 列出最近的文章
      */
-    PagedResult<Article> listByTime(int limit);
+    PagedResult<Article> listByTime(int limit, PageInfo pageInfo);
 
     /**
      * 列出所有的栏目
      */
-    PagedResult<Channel> listChannel();
+    PagedResult<Channel> listChannel(PageInfo pageInfo);
 
     /**
      * 删除栏目
@@ -61,11 +62,11 @@ public interface CMSService {
     /**
      * 根据标题查找文章
      */
-    Article findByName(String title);
+    Article GetByName(String title);
 
     /**
      * 根据关键字查找文章
      */
-    PagedResult<Article> findByKeyword(String keyword);
+    PagedResult<Article> ListByKeyword(String keyword, PageInfo pageInfo);
 
 }
