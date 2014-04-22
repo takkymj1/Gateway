079b3a7d73cd8a7cd36d2a777b14956bb9477e63
suetming <suetming.ma@gmail.com>
2014-04-22 15:03:11 +0800

    RTHX车贷

--- Contract/src/main/java/com/creditcloud/contract/api/ContractService.java
+++ Contract/src/main/java/com/creditcloud/contract/api/ContractService.java
@@ -46,6 +46,29 @@ public interface ContractService {
                               Loan loan,
                               List<Repayment> repayments,
                               FeeConfig feeConfig,
+                              String templateId);
+    
+    /**
+     * 生成一份普通的平台借款合同.
+     *
+     * 只是触发远程的生成过程，为异步调用
+     *
+     * 重复调用将生成新的合同，原有合同不删除
+     *
+     * 借款合同命名为 loan title + investor + obligator + date，合同名称不是唯一的！
+     *
+     * @param client     平台
+     * @param invest     投资
+     * @param loan       借款对象
+     * @param repayments 还款列表
+     * @param feeConfig  费用配置
+     * @param templateId 合同模板id，为空则使用默认模板或LoanRequest指定的关联模板
+     */
+    void generateLoanContract(Client client,
+                              Invest invest,
+                              Loan loan,
+                              List<Repayment> repayments,
+                              FeeConfig feeConfig,
                               String templateId,
                               Map<String, Object> values);
 
