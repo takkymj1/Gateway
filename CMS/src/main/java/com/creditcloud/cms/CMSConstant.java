3b11ce726db40109db3e8305918aee6f8f7b410a
rooseek <rooseek@gmail.com>
2014-05-08 11:52:43 +0800

    cms article work

---
+++ CMS/src/main/java/com/creditcloud/cms/CMSConstant.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.cms;
+
+/**
+ *
+ * @author rooseek
+ */
+public interface CMSConstant {
+
+    String DEFAULT_ARTICLE_URL = "/cms/article";
+
+    String DEFAULT_CHANNEL_URL = "/cms/channel";
+
+}
