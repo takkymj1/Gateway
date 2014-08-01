110ba92e434396797911fc3a2faea8b0ffe40b17
rooseek <rooseek@gmail.com>
2014-08-01 15:23:54 +0800

    CMNG-164  add option for advance repay when payment undue

--- Service/src/main/java/com/creditcloud/service/InvestRepayService.java
+++ Service/src/main/java/com/creditcloud/service/InvestRepayService.java
@@ -34,6 +34,27 @@ public interface InvestRepayService {
     List<InvestRepayment> listRepayByInvest(String clientCode, String investId);
 
     /**
+     * repayment of a loan, set repayment status and generate fund record
+     *
+     * @param clientCode
+     * @param repayDetails
+     * @param type                  目前只有提前还款用到此参数,也将用于逾期违约等还款处理
+     * @param advanceRepayWhenUndue 如果未到还款日还款是否按照提前还款流程走，false则按正常还款流程
+     * @return
+     */
+    boolean repayLoan(String clientCode, Map<String, Object> repayDetails, RepayType type, boolean advanceRepayWhenUndue);
+
+    /**
+     * 商户垫付支付
+     *
+     * @param clientCode
+     * @param loanRepay
+     * @param type
+     * @return
+     */
+    boolean disburseLoan(String clientCode, LoanRepayment loanRepay, DisburseType type);
+
+    /**
      * 根据贷款第几期所有投资应还款计算总费用，以避免直接从LoanRepayment计算的误差<p>
      * 只计算未还款状态的
      *
@@ -73,4 +94,5 @@ public interface InvestRepayService {
      * @return
      */
     RepayDetail getDisburseDetail(String clientCode, LoanRepayment loanRepay, DisburseType type);
+
 }
