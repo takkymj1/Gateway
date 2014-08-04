473bfaf96533a5c0e0ecdcc36889f3f9a8075e91
rooseek <rooseek@gmail.com>
2014-08-04 11:32:36 +0800

    wrap repay related info in RepayInfo and DisburseInfo

---
+++ Service/src/main/java/com/creditcloud/service/model/RepayInfo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.service.model;
+
+import com.creditcloud.config.FeeConfig;
+import com.creditcloud.model.BaseObject;
+import com.creditcloud.model.enums.loan.RepayType;
+import com.creditcloud.model.loan.LoanRepayment;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author rooseek
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement
+public class RepayInfo extends BaseObject {
+
+    private static final long serialVersionUID = 20140804L;
+
+    private LoanRepayment loanRepayment;
+
+    private FeeConfig feeConfig;
+
+    /**
+     * 目前只有提前还款用到此参数,也将用于逾期违约等还款处理
+     */
+    private RepayType type;
+
+    /**
+     * 如果未到还款日还款是否按照提前还款流程走，false则按正常还款流程
+     */
+    private boolean advanceRepayWhenUndue;
+
+}
