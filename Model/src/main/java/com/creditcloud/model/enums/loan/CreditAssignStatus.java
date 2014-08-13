08d3f8aa5f2cb5dbe293e99daf5b3075f25181f9
rooseek <rooseek@gmail.com>
2014-08-13 21:28:25 +0800

    保理新需求

---
+++ Model/src/main/java/com/creditcloud/model/enums/loan/CreditAssignStatus.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.model.enums.loan;
+
+import com.creditcloud.model.enums.BaseEnum;
+
+/**
+ * 债权转让状态
+ *
+ * @author rooseek
+ */
+public enum CreditAssignStatus implements BaseEnum {
+
+    OPEN("转让中"),
+    FINISHED("转让已满"),
+    FAILED("转让未满"),
+    SETTLED("转让成功"),
+    CANCELED("已取消");
+
+    private final String key;
+
+    private CreditAssignStatus(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+}
