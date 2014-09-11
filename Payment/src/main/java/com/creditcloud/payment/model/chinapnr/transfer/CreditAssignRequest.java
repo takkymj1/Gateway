785f2b85359993846d4dbef1bc800eb8af8f3fe6
rooseek <rooseek@gmail.com>
2014-09-11 13:51:48 +0800

    CMKT-305 auto credit assign

--- Payment/src/main/java/com/creditcloud/payment/model/chinapnr/transfer/CreditAssignRequest.java
+++ Payment/src/main/java/com/creditcloud/payment/model/chinapnr/transfer/CreditAssignRequest.java
@@ -67,7 +67,8 @@ public class CreditAssignRequest extends BaseRequest {
     @Size(max = 512)
     private String ReqExt;
 
-    public CreditAssignRequest(String MerCustId,
+    public CreditAssignRequest(CmdIdType cmdIdType,
+                               String MerCustId,
                                String SellCustId,
                                String CreditAmt,
                                String CreditDealAmt,
@@ -80,7 +81,7 @@ public class CreditAssignRequest extends BaseRequest {
                                String RetUrl,
                                String BgRetUrl,
                                String ReqExt) {
-        super(PnRConstant.Version, CmdIdType.CreditAssign, MerCustId);
+        super(PnRConstant.Version, cmdIdType, MerCustId);
         this.SellCustId = SellCustId;
         this.CreditAmt = CreditAmt;
         this.CreditDealAmt = CreditDealAmt;
