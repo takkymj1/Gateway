a3ffd65492335df69734d4f80802804061c2bb51
rooseek <rooseek@gmail.com>
2014-10-23 16:47:24 +0800

    split AbstractReadDAO from AbstractDAO

---
+++ Common/src/main/java/com/creditcloud/common/entities/dao/AbstractDAO.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.common.entities.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
+
+/**
+ * 抽象的DAO类
+ *
+ * @author sobranie
+ */
+public abstract class AbstractDAO<T> extends AbstractReadDAO<T> {
+
+    public AbstractDAO(Class<T> entityClass) {
+        super(entityClass);
+    }
+
+    /**
+     * create new entity
+     *
+     * @param entity
+     * @return
+     */
+    public T create(T entity) {
+        EntityManager em = getEntityManager();
+        em.persist(entity);
+        em.flush();
+        em.refresh(entity, LockModeType.PESSIMISTIC_READ);
+        return entity;
+    }
+
+    /**
+     * update entity, create new if not exist
+     *
+     * @param entity
+     */
+    public void edit(T entity) {
+        getEntityManager().merge(entity);
+    }
+
+    /**
+     * remote entity
+     *
+     * @param entity
+     */
+    public void remove(T entity) {
+        EntityManager em = getEntityManager();
+        em.remove(em.merge(entity));
+    }
+
+    /**
+     * remove entity by unique id
+     *
+     * @param id
+     */
+    public void removeById(Object id) {
+        EntityManager em = getEntityManager();
+        T t = em.find(entityClass, id);
+        if (t != null) {
+            em.remove(em.merge(t));
+        }
+    }
+}
