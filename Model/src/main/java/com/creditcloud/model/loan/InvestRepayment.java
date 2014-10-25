e7180447a40fb45cc0fa766ea4914efd884549f2
rooseek <rooseek@gmail.com>
2014-10-25 18:33:12 +0800

    CMNG-205 addition for Lending

--- Model/src/main/java/com/creditcloud/model/loan/InvestRepayment.java
+++ Model/src/main/java/com/creditcloud/model/loan/InvestRepayment.java
@@ -24,51 +24,42 @@ public class InvestRepayment extends BaseObject {
     private static final long serialVersionUID = 20130918L;
 
     @NotNull
-    @Getter
-    private final String id;
+    private String id;
 
     @NotNull
-    @Getter
-    private final Invest invest;
+    private Invest invest;
+
+    private String investId;
 
     //当前回款期数
     @NotNull
-    @Getter
-    private final int currentPeriod;
+    private int currentPeriod;
 
     //当期回款
     @NotNull
-    @Getter
-    private final Repayment repayment;
+    private Repayment repayment;
 
     //回款状态
     @NotNull
-    @Getter
-    private final RepaymentStatus status;
+    private RepaymentStatus status;
 
     //实际回款
-    @Getter
-    private final BigDecimal repayAmount;
+    private BigDecimal repayAmount;
 
     //实际回款日期
-    @Getter
-    private final Date repayDate;
+    private Date repayDate;
 
     /**
      * 还款来源，实际还款人，如果为null则为loan中借款人，还可以为平台或者担保公司等
      */
-    @Getter
-    private final RealmEntity repaySource;
+    private RealmEntity repaySource;
 
     /**
      * 相对期数,例如对于债券转让的还款,原始期数为12期，从第5期承接转让，那么currentPeriod为5-12，relativePeiod为1-8
      */
     @Setter
-    @Getter
     private int relativePeriod;
-    
-    @Getter
-    @Setter
+
     //TODO replace with <FundRecordType, BigDecimal>
     private Map<String, BigDecimal> feeDetail;
 
@@ -88,5 +79,13 @@ public class InvestRepayment extends BaseObject {
         this.repayAmount = repayAmount;
         this.repayDate = repayDate;
         this.repaySource = repaySource;
+    }
+
+    public String getInvestId() {
+        if (investId != null) {
+            return investId;
+        }
+
+        return invest == null ? null : invest.getId();
     }
 }
