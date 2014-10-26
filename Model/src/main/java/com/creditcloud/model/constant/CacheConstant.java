97aacc8aab30007dc4d0c97b65c8c4c446690ba1
suetming <suetming.ma@gmail.com>
2014-10-26 12:39:48 +0800

    creditmanager dashboard cache

---
+++ Model/src/main/java/com/creditcloud/model/constant/CacheConstant.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.model.constant;
+
+/**
+ *
+ * @author suetming
+ */
+public interface CacheConstant {
+    
+    /**
+     * define cache key for inner cache
+     */
+    String KEY_PREFIX_INNER_CREDITMANAGER = "CreditManager_%1$s";
+    
+    
+}
