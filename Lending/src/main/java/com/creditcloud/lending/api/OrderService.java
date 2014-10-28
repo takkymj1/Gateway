ced64ed783db64a13ba8aa3efffa09e1882d1719
rooseek <rooseek@gmail.com>
2014-10-28 22:34:10 +0800

    CMNG-205 add OrderService and lending and clean

---
+++ Lending/src/main/java/com/creditcloud/lending/api/OrderService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.lending.api;
+
+import com.creditcloud.lending.model.LoanOrder;
+import com.creditcloud.lending.model.RepayOrder;
+import com.creditcloud.model.enums.TransStat;
+import com.creditcloud.model.loan.Invest;
+import com.creditcloud.model.loan.InvestRepayment;
+import java.util.List;
+import java.util.Map;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author rooseek
+ */
+@Remote
+public interface OrderService {
+
+    boolean markLoanOrderStat(TransStat stat, String orderId);
+
+    boolean markRepayOrderStat(TransStat stat, String orderId);
+
+    Map<String, LoanOrder> listOrderByLoan(String loanId, List<Invest> investList);
+
+    Map<String, RepayOrder> listOrderByRepay(String loanRepayId, List<InvestRepayment> repayments);
+}
