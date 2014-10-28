b343da7fa2ad3eedc39098e5615b4a0442034c76
ceusan <ceus@163.com>
2014-10-28 17:20:14 +0800

    增加保理合同生成接口

--- Contract/src/main/java/com/creditcloud/contract/api/ContractService.java
+++ Contract/src/main/java/com/creditcloud/contract/api/ContractService.java
@@ -5,6 +5,7 @@
  */
 package com.creditcloud.contract.api;
 
+import com.creditcloud.claim.model.Claim;
 import com.creditcloud.config.FeeConfig;
 import com.creditcloud.contract.Contract;
 import com.creditcloud.contract.ContractSeal;
@@ -68,6 +69,35 @@ public interface ContractService {
                               List<ContractSeal> seals);
     
     /**
+     * 生成一份普通的平台借款合同（电子签名和电子签章） for FMAX.
+     *
+     * 只是触发远程的生成过程，为异步调用
+     *
+     * 重复调用将生成新的合同，原有合同不删除
+     *
+     * 借款合同命名为 loan title + investor + obligator + date，合同名称不是唯一的！
+     *
+     * @param client     平台
+     * @param invest     投资
+     * @param loan       借款对象
+     * @param repayments 还款列表
+     * @param feeConfig  费用配置
+     * @param templateId 合同模板id，为空则使用默认模板或LoanRequest指定的关联模板
+     * @param values
+     * @param seals      合同签章
+     * @param claim
+     */
+    void generateLoanContract(Client client,
+                              Invest invest,
+                              Loan loan,
+                              List<Repayment> repayments,
+                              FeeConfig feeConfig,
+                              String templateId,
+                              Map<String, Object> values,
+                              List<ContractSeal> seals,
+                              Claim claim);
+    
+    /**
      * 生成一份普通的平台借款合同(平台和借款人签订的合同).
      *
      * 只是触发远程的生成过程，为异步调用
@@ -112,6 +142,31 @@ public interface ContractService {
                               FeeConfig feeConfig,
                               String templateId);
     
+     /**
+     * 生成一份普通的平台借款合同.
+     *
+     * 只是触发远程的生成过程，为异步调用
+     *
+     * 重复调用将生成新的合同，原有合同不删除
+     *
+     * 借款合同命名为 loan title + investor + obligator + date，合同名称不是唯一的！
+     *
+     * @param client     平台
+     * @param invest     投资
+     * @param loan       借款对象
+     * @param repayments 还款列表
+     * @param feeConfig  费用配置
+     * @param templateId 合同模板id，为空则使用默认模板或LoanRequest指定的关联模板
+     * @param claim
+     */
+    void generateLoanContract(Client client,
+                              Invest invest,
+                              Loan loan,
+                              List<Repayment> repayments,
+                              FeeConfig feeConfig,
+                              String templateId,
+                              Claim claim);
+    
     /**
      * 生成一份普通的平台借款合同.
      *
@@ -136,6 +191,33 @@ public interface ContractService {
                               FeeConfig feeConfig,
                               String templateId,
                               Map<String, Object> values);
+    
+     /**
+     * 生成一份普通的平台借款合同.
+     *
+     * 只是触发远程的生成过程，为异步调用
+     *
+     * 重复调用将生成新的合同，原有合同不删除
+     *
+     * 借款合同命名为 loan title + investor + obligator + date，合同名称不是唯一的！
+     *
+     * @param client     平台
+     * @param invest     投资
+     * @param loan       借款对象
+     * @param repayments 还款列表
+     * @param feeConfig  费用配置
+     * @param templateId 合同模板id，为空则使用默认模板或LoanRequest指定的关联模板
+     * @param values
+     * @param claim
+     */
+    void generateLoanContract(Client client,
+                              Invest invest,
+                              Loan loan,
+                              List<Repayment> repayments,
+                              FeeConfig feeConfig,
+                              String templateId,
+                              Map<String, Object> values,
+                              Claim claim);
 
     /**
      * 生成一份普通的平台借款合同(里面会有一份多投资人列表).
@@ -163,6 +245,33 @@ public interface ContractService {
                               String templateId);
     
     /**
+     * 生成一份普通的平台借款合同(里面会有一份多投资人列表).
+     *
+     * 只是触发远程的生成过程，为异步调用
+     *
+     * 重复调用将生成新的合同，原有合同不删除
+     *
+     * 借款合同命名为 loan title + obligator + date，合同名称不是唯一的！
+     *
+     * @param client     平台
+     * @param currentInvest     当前投资人投标
+     * @param investList 所有投资人投资列表
+     * @param loan       借款对象
+     * @param repayments 借款人还款列表
+     * @param feeConfig  费用配置
+     * @param templateId 合同模板id，为空则使用默认模板或LoanRequest指定的关联模板
+     * @param claim
+     */
+    void generateLoanContract(Client client,
+                              Invest currentInvest,
+                              List<Invest> investList,
+                              Loan loan,
+                              List<LoanRepayment> repayments,
+                              FeeConfig feeConfig,
+                              String templateId,
+                              Claim claim);
+    
+    /**
      * 生成一份普通的平台借款合同(生成一份多个投资人对借款人).
      *
      * 只是触发远程的生成过程，为异步调用
@@ -184,6 +293,31 @@ public interface ContractService {
                               List<LoanRepayment> repayments,
                               FeeConfig feeConfig,
                               String templateId);
+    
+    /**
+     * 生成一份普通的平台借款合同(生成一份多个投资人对借款人).
+     *
+     * 只是触发远程的生成过程，为异步调用
+     *
+     * 重复调用将生成新的合同，原有合同不删除
+     *
+     * 借款合同命名为 loan title + obligator + date，合同名称不是唯一的！
+     *
+     * @param client     平台
+     * @param investList 投资列表
+     * @param loan       借款对象
+     * @param repayments 借款人还款列表
+     * @param feeConfig  费用配置
+     * @param templateId 合同模板id，为空则使用默认模板或LoanRequest指定的关联模板
+     * @param claim
+     */
+    void generateLoanContract(Client client,
+                              List<Invest> investList,
+                              Loan loan,
+                              List<LoanRepayment> repayments,
+                              FeeConfig feeConfig,
+                              String templateId,
+                              Claim claim);
 
     /**
      * 读取借款协议.
