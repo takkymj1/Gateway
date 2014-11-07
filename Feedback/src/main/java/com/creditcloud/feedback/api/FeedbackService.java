cee982b9d3dc6e94bb614c7b6ff776ea8108d21f
suetming <suetming.ma@gmail.com>
2014-11-07 12:29:09 +0800

    feedback api

--- Feedback/src/main/java/com/creditcloud/feedback/api/FeedbackService.java
+++ Feedback/src/main/java/com/creditcloud/feedback/api/FeedbackService.java
@@ -17,8 +17,35 @@ import javax.ejb.Remote;
 @Remote
 public interface FeedbackService {
  
+    /**
+     * list feedback by page info
+     * @param clientCode
+     * @param pageInfo
+     * @return 
+     */
     public List<Feedback> listFeedback(String clientCode, PageInfo pageInfo);
     
+    /**
+     * add feedback
+     * @param clientCode
+     * @param userId
+     * @param contact
+     * @param feedback 
+     */
     public void addFeedback(String clientCode, String userId, String contact, String feedback);
+    
+    /**
+     * get feedback list size
+     * @param clientCode
+     * @return 
+     */
+    public long getTotalSize(String clientCode);
+    
+    /**
+     * remove feedback from list
+     * @param clientCode
+     * @param feedback 
+     */
+    public void remove(String clientCode, Feedback feedback);
     
 }
