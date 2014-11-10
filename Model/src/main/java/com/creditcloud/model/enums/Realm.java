681d448a89e9ea82e719157aa5f12aa099c121e6
suetming <suetming.ma@gmail.com>
2014-11-10 22:15:02 +0800

    新增合同章实体关联合同模板、特征章关联企业模板

--- Model/src/main/java/com/creditcloud/model/enums/Realm.java
+++ Model/src/main/java/com/creditcloud/model/enums/Realm.java
@@ -91,6 +91,7 @@ public enum Realm implements BaseEnum {
     MOBILE("手机号"),
     CONTRACT("合同"),
     CONTRACTTEMPLATE("合同模板"),
+    CONTRACTSEAL("合同章"),
     IMAGE("图片"),
     FILE("文件"),
     FSS("生利宝"),
