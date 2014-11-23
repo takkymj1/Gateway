8c75638cd0ffb25b57e6665f7279668e0a188790
rooseek <rooseek@gmail.com>
2014-11-23 22:07:19 +0800

    tiny rename

--- Yeep/src/main/java/com/creditcloud/yeep/enums/Status.java
+++ Yeep/src/main/java/com/creditcloud/yeep/enums/Status.java
@@ -6,12 +6,19 @@
 package com.creditcloud.yeep.enums;
 
 /**
+ * 订单状态
  *
  * @author rooseek
  */
-public enum YeepAction {
+public enum Status {
 
-    toRegister;
-    //TODO more
-
+    INIT,
+    SUCCESS,
+    FREEZED,
+    PREAUTH,//已授权
+    CONFIRM,//已确认出款
+    CANCEL,//已取消
+    PROCESSING,//处理中
+    ERROR,//异常
+    FAIL,//失败
 }
