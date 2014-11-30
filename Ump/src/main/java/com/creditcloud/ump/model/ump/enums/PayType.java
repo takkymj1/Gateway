b9fdbb59c66effb37ca40ab61e1314d957e7ea3b
Min Chen <sobranie9@gmail.com>
2014-11-30 11:33:03 +0800

    revise enum

--- Ump/src/main/java/com/creditcloud/ump/model/ump/enums/PayType.java
+++ Ump/src/main/java/com/creditcloud/ump/model/ump/enums/PayType.java
@@ -3,15 +3,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.creditcloud.ump.model.ump.enums;
+
+import com.creditcloud.model.enums.BaseEnum;
 
 /**
  *
  * @author kdliu
  */
-public enum PayType {
-    B2CDEBITBANK,
-    B2BBANK,
-    DEBITCARD;
+public enum PayType implements BaseEnum {
+    /**
+     * 借记卡网银
+     */
+    B2CDEBITBANK("借记卡网银"),
+    /**
+     * 企业网银
+     */
+    B2BBANK("企业网银"),
+    /**
+     * 借记卡快捷
+     */
+    DEBITCARD("借记卡快捷");
+    
+    private final String key;
+    
+    private PayType(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+    
 }
