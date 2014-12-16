23d63aa337e3d0a71ea115e93ea9874156897629
sabercavalry <287967943@qq.com>
2014-12-16 14:10:04 +0800

    Update FeeMode.java

--- Yeep/src/main/java/com/creditcloud/yeep/enums/FeeMode.java
+++ Yeep/src/main/java/com/creditcloud/yeep/enums/FeeMode.java
@@ -12,14 +12,14 @@ import com.creditcloud.model.enums.BaseEnum;
  *
  * @author rooseek
  */
-public enum FeeModel implements BaseEnum {
+public enum FeeMode implements BaseEnum {
 
     PLATFORM("收取商户手续费"),
     USER("收取用户手续费");
 
     private final String key;
 
-    private FeeModel(String key) {
+    private FeeMode(String key) {
         this.key = key;
     }
 
