307d075220eb83fbce60f8fe5fadb79f86a542fb
guangfujia <wulfgang@163.com>
2014-12-24 02:47:08 +0800

    添加一种还款结果类型

--- Model/src/main/java/com/creditcloud/model/enums/loan/RepayLoanResult.java
+++ Model/src/main/java/com/creditcloud/model/enums/loan/RepayLoanResult.java
@@ -13,6 +13,7 @@ public enum RepayLoanResult implements BaseEnum {
     FAIL("失败"),
     INSUFFICIENT("余额不足"),
     GUARANTEE_NOT_FOUND("找不到担保账户"),
+    GUARANTEE_UMP_NOT_FOUND("U付账户异常"),
     ILLEGAL_PARAM("参数非法");
 
     private final String key;
