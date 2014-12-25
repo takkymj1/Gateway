dad97bba3a5d1ffb47dabcea171dc8349c2096c7
rooseek <hao.chen@creditcloud.com>
2014-12-25 15:24:05 +0800

    联动字段说明

--- Ump/src/main/java/com/creditcloud/ump/model/UmpUser.java
+++ Ump/src/main/java/com/creditcloud/ump/model/UmpUser.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.creditcloud.ump.model;
 
 import com.creditcloud.model.BaseObject;
@@ -21,31 +20,33 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @XmlRootElement
-public class UmpUser extends BaseObject{
-    
+public class UmpUser extends BaseObject {
+
     private String umpUserId;
-    
+
     private String umpAccountId;
-    
+
     private String userName;
-    
+
     private String identityType;
-    
+
     private String identityCode;
-    
+
     private String mobileId;
-    
+
     private String email;
-    
+
+    /**
+     * 1-正常 2-挂失 3-冻结 4-锁定 9-销户
+     */
     private String accountState;
-    
+
     private BigDecimal balance;
-    
+
     private String cardId;
-    
+
     private String gateId;
-    
+
     private String userBindAgreementList;
-    
-    
+
 }
