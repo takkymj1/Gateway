9c133a7b6167c788d942109987b90b4d68cf697e
rooseek <rooseek@gmail.com>
2015-01-07 13:26:05 +0800

    fix QueryUsrInfo and QueryTransDetail

--- Payment/src/main/java/com/creditcloud/payment/model/chinapnr/query/QueryTransDetailResponse.java
+++ Payment/src/main/java/com/creditcloud/payment/model/chinapnr/query/QueryTransDetailResponse.java
@@ -5,69 +5,80 @@
  */
 package com.creditcloud.payment.model.chinapnr.query;
 
-
 import com.creditcloud.payment.model.chinapnr.base.UserResponse;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.ws.rs.FormParam;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 
 /**
  *
- * @author kakaci
+ * @author rooseek
  */
 @Data
 @NoArgsConstructor
-public class QueryTransDetailResponse extends UserResponse{
+public class QueryTransDetailResponse extends UserResponse {
+
+    @FormParam("OrdId")
     @NotNull
     @Size(max = 20)
     private String OrdId;
-    
+
+    @FormParam("OrdDate")
     @NotNull
-    @Size(max = 8,min=8)
+    @Size(max = 8, min = 8)
     private String OrdDate;
-    
+
+    @FormParam("QueryTransType")
     @NotNull
     private String QueryTransType;
-    
+
+    @FormParam("TransAmt")
     @NotNull
     @Size(max = 14)
     private String TransAmt;
-    
+
+    @FormParam("TransStat")
     @NotNull
-    @Size(max = 1,min=1)
+    @Size(max = 1, min = 1)
     private String TransStat;
-    
+
+    @FormParam("FeeAmt")
     @Size(max = 14)
     private String FeeAmt;
-    
+
+    @FormParam("FeeCustId")
     @Size(max = 16)
     private String FeeCustId;
-    
+
+    @FormParam("FeeAcctId")
     @Size(max = 9)
     private String FeeAcctId;
-    
+
+    @FormParam("GateBusiId")
     @Size(max = 6)
     private String GateBusiId;
-    
+
+    @FormParam("RespExt")
     @Size(max = 512)
     private String RespExt;
-    
+
     @Override
-    public  String chkString(){
-        StringBuilder sb=new StringBuilder(super.baseChkString());
-        sb.append(StringUtils.trimToEmpty(this.getOrdId())).
-                append(StringUtils.trimToEmpty(this.getOrdDate())).
-                append(StringUtils.trimToEmpty(this.getQueryTransType())).
-                append(StringUtils.trimToEmpty(this.getTransAmt())).
-                append(StringUtils.trimToEmpty(this.getTransStat())).
-                append(StringUtils.trimToEmpty(this.getFeeAmt())).
-                append(StringUtils.trimToEmpty(this.getFeeCustId())).
-                append(StringUtils.trimToEmpty(this.getFeeAcctId())).
-                append(StringUtils.trimToEmpty(this.getGateBusiId())).
-                append(StringUtils.trimToEmpty(this.getRespExt()));
+    public String chkString() {
+        StringBuilder sb = new StringBuilder(super.baseChkString());
+        sb.append(StringUtils.trimToEmpty(getOrdId())).
+                append(StringUtils.trimToEmpty(getOrdDate())).
+                append(StringUtils.trimToEmpty(getQueryTransType())).
+                append(StringUtils.trimToEmpty(getTransAmt())).
+                append(StringUtils.trimToEmpty(getTransStat())).
+                append(StringUtils.trimToEmpty(getFeeAmt())).
+                append(StringUtils.trimToEmpty(getFeeCustId())).
+                append(StringUtils.trimToEmpty(getFeeAcctId())).
+                append(StringUtils.trimToEmpty(getGateBusiId())).
+                append(StringUtils.trimToEmpty(getRespExt()));
         return sb.toString();
     }
-    
+
 }
