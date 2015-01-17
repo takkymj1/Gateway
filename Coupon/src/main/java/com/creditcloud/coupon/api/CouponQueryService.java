4abada34a3a8ca5313f7313628bc441a358cd929
Min Chen <sobranie9@gmail.com>
2015-01-18 05:52:46 +0800

    完善 Coupon

--- Coupon/src/main/java/com/creditcloud/coupon/api/CouponQueryService.java
+++ Coupon/src/main/java/com/creditcloud/coupon/api/CouponQueryService.java
@@ -19,36 +19,48 @@ import javax.ejb.Remote;
  */
 @Remote
 public interface CouponQueryService {
+    
+    /**
+     * 根据Id获取奖券包
+     * 
+     * @param clientCode
+     * @param id
+     * @return 
+     */
+     public CouponPackage findCouponPackagebyId(String clientCode, String id);
 
     /**
      * 分页列出所有的奖券包
      * 
+     * @param clientCode
      * @param pageInfo
      * @return 
      */
-    PagedResult<CouponPackage> listCouponPackage(PageInfo pageInfo);
+    PagedResult<CouponPackage> listCouponPackage(String clientCode, PageInfo pageInfo);
     
     /**
      * 分页列出某个人的所有奖券.
      * 
      * 第一排序是状态，第二排序是获得时间
      * 
+     * @param clientCode
      * @param userId
      * @param pageInfo
      * @return 
      */
-    PagedResult<CouponPlacement> listUserPlacement(String userId, PageInfo pageInfo);
+    PagedResult<CouponPlacement> listUserPlacement(String clientCode, String userId, PageInfo pageInfo);
     
     /**
      * 按照给定状态分页列出某个人的奖券.
      * 
      * 排序按照获得时间倒序
      * 
+     * @param clientCode
      * @param userId
      * @param statusList
      * @param pageInfo
      * @return 
      */
-    PagedResult<CouponPlacement> listUserPlacementByStatus(String userId, PageInfo pageInfo, CouponStatus... statusList);
+    PagedResult<CouponPlacement> listUserPlacementByStatus(String clientCode, String userId, PageInfo pageInfo, CouponStatus... statusList);
     
 }
