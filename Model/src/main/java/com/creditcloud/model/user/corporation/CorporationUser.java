e562cf1285a4ce1637cc4f5c5d2bbaa7a1be2ad1
tinglany <lanyuntai@gmail.com>
2015-02-03 19:46:53 +0800

    update getCorporationDTO

--- Model/src/main/java/com/creditcloud/model/user/corporation/CorporationUser.java
+++ Model/src/main/java/com/creditcloud/model/user/corporation/CorporationUser.java
@@ -105,7 +105,8 @@ public class CorporationUser extends BaseObject {
                            CorporationType type,
                            String category,
                            String legalPersonId,
-                           Boolean rtpo) {
+                           Boolean rtpo,
+                           String bankLicense) {
         this.user = user;
         this.name = name;
         this.shortName = shortName;
