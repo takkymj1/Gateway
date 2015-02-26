682632f7d381fd35cd47e50cd4c6a92ab8767a9f
zhanggm <zgm05060815@126.com>
2015-02-26 15:56:23 +0800

    债权转让站内信相关model

---
+++ FengMessage/src/main/java/com/creditcloud/fengMessage/model/base/BaseRequest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.creditcloud.fengMessage.model.base;
+
+import com.creditcloud.model.BaseObject;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author zhanggm
+ */
+@Data
+@NoArgsConstructor
+public class BaseRequest  extends BaseObject {
+    
+   protected String template_id;
+}
