f207fceb83aa0a6b347184dbc737ffe32f9e322c
guangfujia <guangfujia@fengjr.com>
2015-03-05 16:46:33 +0800

    修改项目详情页企业借款标的时的认证信息列表

--- Model/src/main/java/com/creditcloud/model/enums/user/credit/CertificateType.java
+++ Model/src/main/java/com/creditcloud/model/enums/user/credit/CertificateType.java
@@ -14,7 +14,7 @@ import java.util.List;
  * @author rooseek
  */
 public enum CertificateType implements BaseEnum {
-
+    //个人认证
     ID("身份认证"),//身份证,户口本,驾照,结婚证等个人身份信息
     CREDITREPORT("信用报告"),//个人信用报告
     FAMILY("家庭情况认证"),//家庭情况  可以上传家庭人员身份证明信息
@@ -29,12 +29,23 @@ public enum CertificateType implements BaseEnum {
     FACTORING("保理认证"),//跟借款挂钩
     CROWDFUNDING("众筹相关"),//跟众筹挂钩
     OTHERS("其他认证"),
-    
+    //收益权转让认证
     ORIGINALAGREEMENT("原始协议认证"),
     BANKTRADE("银行交易凭证"),
     OWNERSHIPREGIST("权属登记认证"),
     FINANCIERIDENTITY("融资人身份认证"),
-    GRANTERIDENTITY("出让方身份认证");
+    GRANTERIDENTITY("出让方身份认证"),
+    //企业认证
+    GRANTERIDFIELD("保障机构实地认证"),
+    BUSINESSLICENCE("营业执照"),
+    TAXREGISTRATION("税务登记证"),
+    BANKOPENACCLICENCE("银行开户许可证"),
+    ORGANIZATIONCODECERT("组织机构代码证"),
+    ARTICLEASSOCIATION("公司章程"),
+    ENTERPRISECREDITREPORT("企业征信报告"),
+    ENTERPRISEBANKWATER("企业银行流水"),
+    OWNPROPERTY("企业自有房产认证"),
+    LEGALPERSONID("法定代表人身份证");
     
     private final String key;
 
