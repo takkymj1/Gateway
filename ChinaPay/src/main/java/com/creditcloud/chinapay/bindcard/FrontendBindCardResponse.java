016dfbfb9d19545149b32b2596f4593140ded410
rooseek <rooseek@gmail.com>
2015-03-11 17:53:56 +0800

    add FrontEndBindCardResponse

---
+++ ChinaPay/src/main/java/com/creditcloud/chinapay/bindcard/FrontendBindCardResponse.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.chinapay.bindcard;
+
+import com.creditcloud.model.BaseObject;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.FormParam;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 前台页面认证绑卡response
+ *
+ * @author rooseek
+ */
+@Data
+@NoArgsConstructor
+public class FrontendBindCardResponse extends BaseObject {
+
+    private static final long serialVersionUID = 20150311L;
+
+    @FormParam("appSysId")
+    private String appSysId;
+
+    @FormParam("signMethod")
+    private String signMethod;
+
+    @FormParam("usrSysId")
+    private String usrSysId;
+
+    @FormParam("email")
+    private String email;
+
+    @FormParam("mobile")
+    private String mobile;
+
+    @FormParam("pgRetUrl")
+    @NotNull
+    private String pgRetUrl;
+
+    @FormParam("bgRetUrl")
+    private String bgRetUrl;
+
+    @FormParam("ordDate")
+    @NotNull
+    private String ordDate;
+
+    @FormParam("ordSeqId")
+    @NotNull
+    private String ordSeqId;
+
+    @FormParam("cardNo")
+    private String cardNo;
+
+    @FormParam("certType")
+    private String certType;
+
+    @FormParam("certNo")
+    private String certNo;
+
+    @FormParam("usrName")
+    private String usrName;
+
+    @FormParam("cardCvn2")
+    private String cardCvn2;
+
+    @FormParam("cardExpire")
+    private String cardExpire;
+
+    @FormParam("cardPhone")
+    private String cardPhone;
+
+    @FormParam("signature")
+    @NotNull
+    private String signature;
+
+    @FormParam("respcode")
+    @NotNull
+    private String respcode;
+
+    @FormParam("respmsg")
+    private String respmsg;
+
+}
