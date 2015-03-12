6b8ff28ed6890be8b22f62dfb5d11c8d612ba97c
rooseek <rooseek@gmail.com>
2015-03-12 15:40:03 +0800

    add PaymentFeatures

---
+++ Config/src/main/java/com/creditcloud/config/PaymentFeatures.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.config;
+
+import com.creditcloud.model.BaseObject;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 支付渠道统一控制
+ *
+ * @author rooseek
+ */
+@NoArgsConstructor
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PaymentFeatures extends BaseObject {
+
+    private static final long serialVersionUID = 20140729L;
+
+    /**
+     * ChinaPay
+     */
+    @XmlElement(required = false)
+    @Getter
+    private boolean enableChinaPay = false;
+
+}
