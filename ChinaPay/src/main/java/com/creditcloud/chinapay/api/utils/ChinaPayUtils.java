e0a11a7bb29578ba77127b586cd7946ad926af5e
rooseek <rooseek@gmail.com>
2015-03-12 22:25:52 +0800

    add ChinaPayUtils

---
+++ ChinaPay/src/main/java/com/creditcloud/chinapay/api/utils/ChinaPayUtils.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.chinapay.api.utils;
+
+import com.creditcloud.model.constant.NumberConstant;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author rooseek
+ */
+public class ChinaPayUtils {
+
+    /**
+     * 定长12位
+     */
+    private static final int DEFAULT_PAD_LENGTH = 12;
+
+    /**
+     * amount大于等于0
+     *
+     * @param amount
+     * @return 定长12位,精确到分,如amount=0.01则返回000000000001
+     */
+    public static String bigDecimalToChinaPayString(BigDecimal amount) {
+        if (amount == null || amount.signum() == -1) {
+            return null;
+        }
+        amount = amount.setScale(NumberConstant.DEFAULT_SCALE, NumberConstant.ROUNDING_MODE);
+        amount = amount.multiply(new BigDecimal(100));
+
+        return pad(amount.intValue());
+    }
+
+    private static String pad(int number) {
+        String f = "%0" + DEFAULT_PAD_LENGTH + "d";
+        return String.format(f, number);
+    }
+}
