9bc8bf4436cb40987fa65f75e8fae2efb315116a
guohua.zhang <andy.chenus@qq.com>
2015-03-13 12:11:00 +0800

    诺安基金货币基金接口 ： 异步鉴权，同步鉴权，同步开户绑卡，申购，赎回 基本框架搭建和基本流程走通

---
+++ InvestmentFund/src/main/java/com/creditcloud/investmentfund/lionfund/api/moneyfund/model/PlainTextSignaturePair.java
@@ -0,0 +1,19 @@
+package com.creditcloud.investmentfund.lionfund.api.moneyfund.model;
+
+
+public class PlainTextSignaturePair extends POJO {
+	public String plainText = "";
+	public String signature = "";
+
+	public static boolean isNullPair(PlainTextSignaturePair pair) {
+		if (pair == null || pair.plainText == null || pair.signature == null)
+			return true;
+
+		return pair.plainText.isEmpty() && pair.signature.isEmpty();
+	}
+
+        @Override
+	public String toString() {
+		return plainText + "," + signature;
+	}
+}
