2c6bdeaa8ef738b133725b03952b02a5f18b2a1c
rooseek <rooseek@gmail.com>
2015-03-16 18:16:24 +0800

    add Environment

---
+++ Config/src/main/java/com/creditcloud/config/Environment.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.config;
+
+/**
+ * 系統运行环境
+ *
+ * @author rooseek
+ */
+public enum Environment {
+
+    /**
+     * 生产环境
+     */
+    Production,
+    /**
+     * 测试环境
+     */
+    Test,
+    /**
+     * 开发环境
+     */
+    Develop,
+
+}
