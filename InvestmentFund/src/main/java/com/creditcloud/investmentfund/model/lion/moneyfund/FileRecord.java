77afc396a27a62dac34610a6982b0275cfe86471
Guohua.Zhang <andy.zhang.guohua@gmail.com>
2015-03-25 11:44:32 +0800

    keep tracking of original record text

--- InvestmentFund/src/main/java/com/creditcloud/investmentfund/model/lion/moneyfund/FileRecord.java
+++ InvestmentFund/src/main/java/com/creditcloud/investmentfund/model/lion/moneyfund/FileRecord.java
@@ -6,11 +6,18 @@
 package com.creditcloud.investmentfund.model.lion.moneyfund;
 
 import com.creditcloud.model.BaseObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  *
  * @author guohuazhang
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class FileRecord  extends BaseObject{
-    
+    /**
+     * 记录在文件中对应行的完整文本
+     */
+    private String recordText;
 }
