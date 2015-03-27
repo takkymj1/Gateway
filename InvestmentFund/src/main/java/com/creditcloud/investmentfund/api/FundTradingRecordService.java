5d8c329f42a2ed087341160c83fda34d00a9967e
suetming <suetming.ma@gmail.com>
2015-03-27 17:28:30 +0800

    【新华电商】基金交易记录接口

--- InvestmentFund/src/main/java/com/creditcloud/investmentfund/api/FundTradingRecordService.java
+++ InvestmentFund/src/main/java/com/creditcloud/investmentfund/api/FundTradingRecordService.java
@@ -7,6 +7,8 @@
 package com.creditcloud.investmentfund.api;
 
 import com.creditcloud.investmentfund.enums.FundBrand;
+import com.creditcloud.investmentfund.enums.TradingRecordStatus;
+import com.creditcloud.investmentfund.enums.TradingRecordType;
 import com.creditcloud.investmentfund.model.FundTradingRecord;
 import com.creditcloud.model.criteria.PageInfo;
 import com.creditcloud.model.misc.PagedResult;
@@ -37,4 +39,16 @@ public interface FundTradingRecordService {
      * @return 
      */
     public PagedResult<FundTradingRecord> listByUserIdAndBrand(String userId, FundBrand fundBrand, PageInfo pageInfo);
+    
+    /**
+     * 获取基金交易记录
+     * 
+     * @param userId        用户ID
+     * @param type          基金类型
+     * @param status        基金状态
+     * @param fundBrand     基金品牌
+     * @param pageInfo      分页信息
+     * @return 
+     */
+    public PagedResult<FundTradingRecord> listByUserIdAndTypeAndStatusAndBrand(String userId, TradingRecordType type, TradingRecordStatus status, FundBrand fundBrand, PageInfo pageInfo);
 }
