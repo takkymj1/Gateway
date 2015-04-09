0f2ebcebc2db6c2270121f2f9c3aa0b249c7ac0b
guangfujia <guangfujia@fengjr.com>
2015-04-09 17:58:41 +0800

    添加站内信请求类型

---
+++ FengMessage/src/main/java/com/creditcloud/fengMessage/model/WechatLotteryCouponNotice01Request.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.fengMessage.model;
+
+import com.creditcloud.fengMessage.model.base.BaseRequest;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author jiaguangfu
+ */
+@Data
+@NoArgsConstructor
+public class WechatLotteryCouponNotice01Request extends BaseRequest {
+    
+    public String date;
+    
+    public String couponAmount;
+    
+    public String rateLimit;
+    
+    public WechatLotteryCouponNotice01Request(String date, String couponAmount, String rateLimit){
+        this.template_id = "WECHAT_LOTTERY_COUPON_001";
+        this.date = date;
+        this.couponAmount = couponAmount;
+        this.rateLimit = rateLimit;
+    }
+}    
+
+
---
+++ FengMessage/src/main/java/com/creditcloud/fengMessage/model/WechatLotteryCouponNotice02Request.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.fengMessage.model;
+
+import com.creditcloud.fengMessage.model.base.BaseRequest;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author jiaguangfu
+ */
+@Data
+@NoArgsConstructor
+public class WechatLotteryCouponNotice02Request extends BaseRequest {
+    
+    public String date;
+    
+    public String couponAmount;
+    
+    public String rateLimit;
+    
+    public WechatLotteryCouponNotice02Request(String date, String couponAmount, String rateLimit){
+        this.template_id = "WECHAT_LOTTERY_COUPON_002";
+        this.date = date;
+        this.couponAmount = couponAmount;
+        this.rateLimit = rateLimit;
+    } 
+}    
+
