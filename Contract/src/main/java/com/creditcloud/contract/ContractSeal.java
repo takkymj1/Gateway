13e7011ca2f5eed21fe485f76c13fb647c421962
suetming <suetming.ma@gmail.com>
2015-04-14 14:20:14 +0800

    【无锡富盈】非三方接口插入合同章

--- Contract/src/main/java/com/creditcloud/contract/ContractSeal.java
+++ Contract/src/main/java/com/creditcloud/contract/ContractSeal.java
@@ -41,6 +41,8 @@ public class ContractSeal extends BaseObject {
     
     private int y;
     
+    private int scale;
+    
     // 印章
     private Seal seal;
     
@@ -66,12 +68,12 @@ public class ContractSeal extends BaseObject {
      */
     public static ContractSeal generatePersonContractSeal(String code, int page, int x, int y) {
         Seal seal = new Seal(null, code, ContractSealType.PERSONAL, null, null, null, null);
-        return new ContractSeal(null, false, "", page, x, y, seal, null);
+        return new ContractSeal(null, false, "", page, x, y, 100, seal, null);
     }
     
     public static ContractSeal generatePersonContractSeal(String code, String keyword) {
         Seal seal = new Seal(null, code, ContractSealType.PERSONAL, null, null, null, null);
-        return new ContractSeal(null, true, keyword, 0, 0, 0, seal, null);
+        return new ContractSeal(null, true, keyword, 0, 0, 0, 100, seal, null);
     }
     
     /**
@@ -84,12 +86,12 @@ public class ContractSeal extends BaseObject {
      */
     public static ContractSeal generatePersonContractSeal(User user, int page, int x, int y) {
         Seal seal = new Seal(null, null, ContractSealType.PERSONAL, null, user.getId(), user, null);
-        return new ContractSeal(null, false, "", page, x, y, seal, null);
+        return new ContractSeal(null, false, "", page, x, y, 100, seal, null);
     }
     
     public static ContractSeal generatePersonContractSeal(User user, String keyword) {
         Seal seal = new Seal(null, null, ContractSealType.PERSONAL, null, user.getId(), user, null);
-        return new ContractSeal(null, true, keyword, 0, 0, 0, seal, null);
+        return new ContractSeal(null, true, keyword, 0, 0, 0, 100, seal, null);
     }
     
     /**
@@ -102,11 +104,23 @@ public class ContractSeal extends BaseObject {
      */
     public static ContractSeal generateContractSeal(String code, int page, int x, int y) {
         Seal seal = new Seal(null, code, ContractSealType.ENTERPRISE, null, null, null, null);
-        return new ContractSeal(null, false, "", page, x, y, seal, null);
+        return new ContractSeal(null, false, "", page, x, y, 100, seal, null);
     }
     
     public static ContractSeal generateContractSeal(String code, String keyword){
         Seal seal = new Seal(null, code, ContractSealType.ENTERPRISE, null, null, null, null);
-        return new ContractSeal(null, true, keyword, 0, 0, 0, seal, null);
+        return new ContractSeal(null, true, keyword, 0, 0, 0, 100, seal, null);
+    }
+    
+    /**
+     * 通过阈值关键字生成图片
+     * 
+     * @param name      公章图片名字
+     * @param scale     放缩系数
+     * @return 
+     */
+    public static ContractSeal generateContractSeal(String name, int scale){
+        Seal seal = new Seal(null, name, ContractSealType.ENTERPRISE, null, null, null, null);
+        return new ContractSeal(null, true, name, 0, 0, 0, scale, seal, null);
     }
 }
