29aa256e8e8a99a42c7afc0b144ba9d456fd754c
sobranie9 <sobranie9@gmail.com>
2015-04-17 18:10:52 +0800

    修复BirthdayHandler，增加健壮性

--- Common/src/main/java/com/creditcloud/common/taglib/BirthdayHandler.java
+++ Common/src/main/java/com/creditcloud/common/taglib/BirthdayHandler.java
@@ -5,6 +5,7 @@
 package com.creditcloud.common.taglib;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
@@ -13,12 +14,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * 将生日显示为 198*年**月**日
+ * 
  * @author sobranie
  */
 public class BirthdayHandler extends SimpleTagSupport {
 
     static Logger logger = LoggerFactory.getLogger(BirthdayHandler.class);
+    
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
+    
     /**
      * 出生日期格式：1987-01-01
      */
@@ -30,11 +35,19 @@ public class BirthdayHandler extends SimpleTagSupport {
             logger.warn("Invalid birthday is null.");
             return;
         }
+        String content = "";
+        try {
+            logger.debug("Mask birtyday {}", birthday);
+            String year = sdf.format(birthday);
+            if (year.length() == 4) {
+                content = content.concat(year.substring(0, 3)).concat("*年**月**日");
+            }
+        } catch (Exception ex) {
+            logger.error("Error happend when format birthday.{}", birthday, ex);
+            return;
+        }
         JspWriter writer = getJspContext().getOut();
-        logger.debug("Mask birtyday {}", birthday);
-        String year = (birthday.getYear()+1900)+"";
-        writer.write(year, 0, 3);
-        writer.write("*年**月**日");
+        writer.write(content);
     }
 
     public void setBirthday(Date birthday) {
