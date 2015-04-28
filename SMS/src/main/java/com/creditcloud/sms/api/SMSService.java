2fb181756073422a32649151aa0b53b5f1f80660
Wei Yan <yw79641760@gmail.com>
2015-04-28 12:32:52 +0800

    new feature: Add support voice captcha for emay (for JXJR)

--- SMS/src/main/java/com/creditcloud/sms/api/SMSService.java
+++ SMS/src/main/java/com/creditcloud/sms/api/SMSService.java
@@ -24,4 +24,17 @@ public interface SMSService {
      * @param content
      */
     void sendMessage(Client client, SMSType type, String mobile, String... content);
+    
+    /**
+     * 发送语音验证码 for emay
+     * 
+     * emay语音号码限制：最多不要超过6个字符，最少不要小于4个字符;字符必须为0至9的全英文半角数字字符
+     * 
+     * @param client
+     * @param type
+     * @param mobile
+     * @param content
+     */
+    void sendVoice(Client client, SMSType type, String mobile, String... content);
+
 }
