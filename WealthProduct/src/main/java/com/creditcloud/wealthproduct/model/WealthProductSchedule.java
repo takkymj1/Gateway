c594e659ab05c17b88ef453ae807e16ccda3136b
suetming <suetming.ma@gmail.com>
2015-04-29 22:17:24 +0800

    【东方新华】字段设计

--- WealthProduct/src/main/java/com/creditcloud/wealthproduct/model/WealthProductSchedule.java
+++ WealthProduct/src/main/java/com/creditcloud/wealthproduct/model/WealthProductSchedule.java
@@ -31,36 +31,30 @@ public class WealthProductSchedule extends BaseObject {
     /**
      * 预约成立时间
      */
-    @Temporal(TemporalType.TIMESTAMP)
     private Date appointEstablishedime;
     
     /**
      * 预约开始时间
      */
-    @Temporal(TemporalType.TIMESTAMP)
     private Date appointStartTime;
 
     /**
      * 预约结束时间
      */
-    @Temporal(TemporalType.TIMESTAMP)
     private Date appointEndTime;
 
     /**
      * 认购开始时间
      */
-    @Temporal(TemporalType.TIMESTAMP)
     private Date subscribeStartTime;
     
     /**
      * 认购结束时间
      */
-    @Temporal(TemporalType.TIMESTAMP)
     private Date subscribeEndTime;
 
     /**
      * 计息日
      */
-    @Temporal(TemporalType.TIMESTAMP)
     private Date valueStartTime;
 }
