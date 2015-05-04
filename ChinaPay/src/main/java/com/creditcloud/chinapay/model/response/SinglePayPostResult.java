d99f02a4248951919569463b492d5db004c378e7
Guohua.Zhang <andy.zhang.guohua@gmail.com>
2015-05-04 14:02:34 +0800

    include UnionPayService,InterestBearingService into Facilities

--- ChinaPay/src/main/java/com/creditcloud/chinapay/model/response/SinglePayPostResult.java
+++ ChinaPay/src/main/java/com/creditcloud/chinapay/model/response/SinglePayPostResult.java
@@ -64,17 +64,51 @@ public class SinglePayPostResult extends POJO {
 
     /**
      * 返回应答成功
-     * @return 
+     *
+     * @return
      */
     public boolean success() {
         return ChinaPayConstant.SINGLE_PAY_TRANS_SUCCESS_CODE.equals(responseCode);
     }
-    
+
+    //s	成功	交易成功	状态码为小写字母s
+    //2	处理中	交易已接受	
+    //3	处理中	财务已确认	
+    //4	处理中	财务处理中	
+    //5	处理中	已发往银行	ChinaPay已将代付交易发往银行。后续若银行返回结果，该状态会相应更新。
+    //6	失败	银行已退单	银行退单，交易失败。
+    //7	处理中	重汇已提交	
+    //8	处理中	重汇已发送	ChinaPay已将代付交易发往银行。后续若银行返回结果，该状态会相应更新。
+    //9	失败	重汇已退单	银行对重汇的代付交易退单，交易失败。
     /**
      * 交易成功
-     * @return 
+     *
+     * @return
      */
-    public boolean transSuccess(){
+    public boolean transSuccess() {
         return success() && "S".equalsIgnoreCase(getStat());
+    }
+
+    /**
+     * 交易失败
+     *
+     * @return
+     */
+    public boolean transFail() {
+        boolean processOK = success();
+        if (!processOK) {
+            return true;
+        }
+        
+        // respCode=0000,但是 stat 不为s的情况
+        if ("6".equalsIgnoreCase(getStat())) {
+            //6	失败	银行已退单	银行退单，交易失败。
+            return true;
+        } else if ("9".equalsIgnoreCase(getStat())) {
+            //9	失败	重汇已退单	银行对重汇的代付交易退单，交易失败。
+            return true;
+        } else {
+            return false;
+        }
     }
 }
