dde53878800b6a05e2e05d8a65ef695631d7772b
Guohua.Zhang <andy.zhang.guohua@gmail.com>
2015-05-08 15:49:58 +0800

    余额生息用户签约登记接口

---
+++ InterestBearing/src/main/java/com/creditcloud/interestbearing/model/UserRegistrationInfo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.interestbearing.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ *
+ * @author guohuazhang
+ */
+@Data
+public class UserRegistrationInfo {
+
+    // 注册时需要提供的参数
+    // 要开通余额生息的用户的ID
+    private String userId;
+    // 开通时间，由调用者设置
+    private Date registrationTime;
+    // 注册时的余额生息资金数量
+    private BigDecimal interestBearingFundAmount;
+    // 用户发起注册的IP 
+    String clientIP;
+    
+    // 注册后查询时会返回的信息
+    // 余额生息签约记录ID
+    private String recordId; 
+}
