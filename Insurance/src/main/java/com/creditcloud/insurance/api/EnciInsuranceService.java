67bcecf8dd00e1da89f94d483ecd67458f62ea74
chaishufan <idexin.chai@creditcloud.com>
2015-05-08 14:39:50 +0800

    XHDS,  添加修改订单状态方法

--- Insurance/src/main/java/com/creditcloud/insurance/api/EnciInsuranceService.java
+++ Insurance/src/main/java/com/creditcloud/insurance/api/EnciInsuranceService.java
@@ -126,6 +126,15 @@ public interface EnciInsuranceService extends EnciInsuranceStatService{
     public boolean markStatus(String clientCode, String OrderId, InsuranceOrderStatus status);
     
     /**
+     * 修改保险订单的状态为指定状态
+     * @param clientCode
+     * @param orderId
+     * @param status
+     * @return 
+     */
+    public void editStatus(String clientCode, String orderId, InsuranceOrderStatus status);
+    
+    /**
      * 加密
      * 
      * @param plainText
