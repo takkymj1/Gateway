85de90aa3275e45d8f928641b2acd2bd7dfa5d6e
Guohua.Zhang <andy.zhang.guohua@gmail.com>
2015-05-08 19:30:45 +0800

    向TA查询平台基金缺口数据结构

---
+++ InterestBearing/src/main/java/com/creditcloud/interestbearing/ta/message/asset/QueryPlatformAssetGapRequestMessage.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.interestbearing.ta.message.asset;
+
+import com.creditcloud.interestbearing.ta.message.RequestMessage;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ *
+ * @author guohuazhang
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class QueryPlatformAssetGapRequestMessage extends RequestMessage {
+
+    private String tenantId;
+    /**
+     * 2015-05-04T17:40:42
+     */
+    private String checkDate;
+}
---
+++ InterestBearing/src/main/java/com/creditcloud/interestbearing/ta/model/ProductAssetGapMetadata.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.creditcloud.interestbearing.ta.model;
+
+import lombok.Data;
+
+/**
+ *
+ * @author guohuazhang
+ */
+@Data
+public class ProductAssetGapMetadata {
+
+    // 资产类型
+    private String type;
+    //id为资产id, 例子中为基金代码
+    private String id;
+    // 当前差额, -为缺口 需要外部购买, + 为节余 ,可能需要赎回
+    private String gap;
+    // 检查时间
+    private String checkDate;
+}
