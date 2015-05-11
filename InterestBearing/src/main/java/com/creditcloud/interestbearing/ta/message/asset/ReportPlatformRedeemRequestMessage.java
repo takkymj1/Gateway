2cce4a35c760d23ca59e569ad4b20072f38dd068
Guohua.Zhang <andy.zhang.guohua@gmail.com>
2015-05-11 17:03:47 +0800

    TA用户申购赎回登记查询

--- InterestBearing/src/main/java/com/creditcloud/interestbearing/ta/message/asset/ReportPlatformRedeemRequestMessage.java
+++ InterestBearing/src/main/java/com/creditcloud/interestbearing/ta/message/asset/ReportPlatformRedeemRequestMessage.java
@@ -9,6 +9,7 @@ import com.creditcloud.interestbearing.ta.message.RequestMessage;
 import java.math.BigDecimal;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.joda.time.LocalDateTime;
 
 /**
  *
@@ -17,11 +18,16 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ReportPlatformRedeemRequestMessage extends RequestMessage {
+
     private String product_type;// 增持或减持的产品类型，如：基金fund
     private String ori_id;// 产品原有代码，如：基金代码
     private BigDecimal hold_num_inc;// 增持份数
     private BigDecimal hold_amount_inc;// 增持价值总金额
     private BigDecimal hold_num_dec;// 减持份数
     private BigDecimal hold_amount_dec;// 减持价值总金额
-    private long act_time;// 增持或减持成功时间
+    private String act_time;// 增持或减持成功时间
+
+    public void setAct_time(LocalDateTime actTime) {
+        act_time = actTime == null ? "" : actTime.toString();
+    }
 }
