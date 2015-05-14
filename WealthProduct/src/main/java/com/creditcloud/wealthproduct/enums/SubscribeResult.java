accf991efe534f526cb40db5007e561ab2fd54e0
suetming <suetming.ma@gmail.com>
2015-05-14 18:20:47 +0800

    【东方新华】字段设计

--- WealthProduct/src/main/java/com/creditcloud/wealthproduct/enums/SubscribeResult.java
+++ WealthProduct/src/main/java/com/creditcloud/wealthproduct/enums/SubscribeResult.java
@@ -13,10 +13,10 @@ import com.creditcloud.model.enums.BaseEnum;
  */
 public enum SubscribeResult implements BaseEnum {
 
-    SUCCESSFUL("申购成功"),
-    EXCEED_TIMES_LIMIT("申购次数超过上限"),
-    NO_BALANCE("申购已满"),
-    FAIL("申购失败");
+    SUCCESSFUL("认购成功"),
+    EXCEED_TIMES_LIMIT("认购次数超过上限"),
+    NO_BALANCE("认购已满"),
+    FAIL("认购失败");
 
     private final String key;
 
