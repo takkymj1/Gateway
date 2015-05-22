30db8e149ecf1aa08e0053a755353dd2638be077
Guohua.Zhang <andy.zhang.guohua@gmail.com>
2015-05-22 15:15:51 +0800

    支持B端的基金申购赎回查询，多基金公司支持

--- InterestBearing/src/main/java/com/creditcloud/interestbearing/api/InterestingBearingPlatformFundTradingService.java
+++ InterestBearing/src/main/java/com/creditcloud/interestbearing/api/InterestingBearingPlatformFundTradingService.java
@@ -6,6 +6,7 @@
 package com.creditcloud.interestbearing.api;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import javax.ejb.Remote;
 
 /**
@@ -15,10 +16,31 @@ import javax.ejb.Remote;
 @Remote
 public interface InterestingBearingPlatformFundTradingService {
 
-    // 申购
+    /**
+     * 申购
+     *
+     * @param broker
+     * @param fundCode
+     * @param amount
+     */
+    void purchase(String broker, String fundCode, BigDecimal amount);
 
-    void purchase(String broker, String fundId, BigDecimal amount);
-    // 赎回
+    /**
+     * 赎回
+     *
+     * @param broker
+     * @param fundCode
+     * @param amount
+     */
 
-    void redeem(String broker, String fundId,BigDecimal amount);
+    void redeem(String broker, String fundCode, BigDecimal amount);
+
+    /**
+     * 查询某段时间内未确认状态的申购，如果这些记录的最终状态可获得，则作相应的更新
+     *
+     * @param broker
+     * @param startDate
+     * @param endDate
+     */
+    void queryAndUpdatePendingTradingRecords(String broker, Date startDate, Date endDate);
 }
